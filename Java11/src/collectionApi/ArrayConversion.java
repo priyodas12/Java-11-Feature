a39e5b14e8846e228a3a63423fb321dcc5a02ee6
Priyobrato Das <priyodas12@gmail.com>
2021-02-08 19:18:18 +0530

    69

--- Java11/src/collectionApi/ArrayConversion.java
+++ Java11/src/collectionApi/ArrayConversion.java
@@ -1,6 +1,7 @@
 package collectionApi;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrayConversion {
@@ -11,6 +12,16 @@ public class ArrayConversion {
         numbers.add(4);
         numbers.add(7);
 
+        System.out.println(numbers);
+        // Java 8 Approach with lambda expression;
+        Integer[] arrayOfNumbersJ8=numbers.stream().toArray(value -> new Integer[numbers.size()]);
+        System.out.println(Arrays.toString(arrayOfNumbersJ8));
+
+        // Java 11 Approach;
+        Integer[] arrayOfNumbersJ11=numbers.toArray(value -> new Integer[value]);
+        // or using method reference;
+        arrayOfNumbersJ11=numbers.toArray(Integer[]::new);
+        System.out.println(Arrays.toString(arrayOfNumbersJ11));
 
     }
 }
