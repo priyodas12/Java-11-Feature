335150746334e9c6d6ef3c91dabc5d648451946f
Priyobrato Das <priyodas12@gmail.com>
2021-02-08 19:06:50 +0530

    68

---
+++ Java11/src/collectionApi/ArrayConversion.java
@@ -0,0 +1,16 @@
+package collectionApi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayConversion {
+    public static void main(String[] args) {
+        List<Integer> numbers=new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(7);
+
+
+    }
+}
