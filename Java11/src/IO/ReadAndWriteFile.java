2a32210921bc3fe740e127ba857fc93f6f6942a3
Priyobrato Das <priyodas12@gmail.com>
2021-02-08 19:31:42 +0530

    70

---
+++ Java11/src/IO/ReadAndWriteFile.java
@@ -0,0 +1,22 @@
+package IO;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class ReadAndWriteFile {
+    public static void main(String[] args) {
+        var path="C:\\Users\\Priyo\\OneDrive\\Desktop\\java11.txt";
+
+        try {
+            Files.writeString(Path.of(path),"\ntesting files data...writing", StandardOpenOption.APPEND);
+
+            String data=Files.readString(Path.of(path));
+            System.out.println(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
